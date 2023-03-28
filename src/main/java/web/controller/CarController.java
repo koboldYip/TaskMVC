ead86fe09cf8ffd153315b7ed2dc05af5012e8e6
Arthur Shmakotin <kyldoks@gmail.com>
2023-03-28 10:21:29 +0300

    Delete Autowired

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,6 +1,5 @@
 package web.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +9,11 @@ import web.service.CarService;
 @Controller
 public class CarController {
 
-    @Autowired
     private CarService carService;
+
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
 
     @GetMapping(value = "/cars")
     public String printCars(@RequestParam(defaultValue = "5") int count, ModelMap model) {
